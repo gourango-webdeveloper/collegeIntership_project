26ea7efd988caf728fb0bbf78a185578ed4d237a
gourango-webdeveloper <gourangodebnath80@gmail.com>
2022-07-01 08:43:55 +0530

    done

---
+++ collegeIntership_project_Nodejs/src/Controller/collegeController.js
@@ -0,0 +1,80 @@
+const CollegeModel = require("../Model/CollegeModel")
+const internModel = require("../Model/internModel")
+const validator = require("../validator/validate")
+
+//-----------------------------------------------[create college]-----------------------------------------------------------------
+const CreateCollege = async function (req, res) {
+  try {
+
+    let data = req.body
+    // name
+    if (!data.name) return res.status(400).send({ status: false, message: "Name is required" });
+    if (!data.name.match(/^[a-zA-Z. ]{2,30}$/)) return res.status(400).send({ status: false, msg: "please enter valid college name" })
+    if (!validator.isValid(data.name)) return res.status(400).send({ status: false, msg: "name is not valid" })
+
+    const uniqueName = await CollegeModel.find({ name: data.name }).select({ name: 1, _id: 0 })
+    if (uniqueName.length) return res.status(400).send({ status: false, msg: "name is already exist" }) //validating uniqueness of Name
+
+    // fullname
+    if (!data.fullName) return res.status(400).send({ status: false, message: "fullName is required" })
+
+
+
+    // logoName
+    if (!validator.isValid(data.logoLink)) return res.status(400).send({ status: false, msg: "LogoName is mandatory" })// validation of url required
+    if (!validator.isUrl(data.logoLink)) return res.status(400).send({ status: false, msg: "enter a valid logoname or url" })
+
+
+    const cData = await CollegeModel.create(data)
+    let obj = {
+      name: cData.name,
+      fullName: cData.fullName,
+      logoLink: cData.logoLink,
+      isDeleted: false
+    }
+
+
+    res.status(201).send({ status: true, data: obj })
+  }
+  catch (err) {
+    res.status(500).send({status:false, msg: err.message })
+  }
+}
+// -----------------------------------------------------[Get College Api]-------------------------------------------------------
+
+const getCollege = async function (req, res) {
+
+  try {
+
+    let filter = req.query
+    //console.log(filter);
+    if (!Object.keys(filter).length) return res.status(200).send({ status: true, msg: "query should be present" });
+
+    let checkCollegeName = await CollegeModel.findOne({ name: filter.collegeName, isDeleted: false }) /*Check College Name From DB*/
+    if (!checkCollegeName) return res.status(404).send({ status: true, msg: "No such college Name found", });
+
+    let collegeId = checkCollegeName._id /*Get CollegeID from CheckCollegeName*/
+    let getAllInternData = await internModel.find({ collegeId: collegeId, isDeleted: false }).select({ name: 1, email: 1, mobile: 1 })
+    if (!getAllInternData.length) return res.status(404).send({ status: false, msg: "No intern Apply for This College", });
+
+    //assign value
+    let name = checkCollegeName.name;
+    let fullName = checkCollegeName.fullName;
+    let logoLink = checkCollegeName.logoLink;
+
+    //call value
+    let collegeData = {
+      name: name,
+      fullName: fullName,
+      logoLink: logoLink,
+      interns: getAllInternData
+    }
+
+    res.status(200).send({ status: true, msg: "got intern Sucessfully", data: collegeData });
+  } catch (err) {
+    res.status(500).send({ status: false, msg: err.message });
+  }
+};
+
+module.exports.CreateCollege = CreateCollege
+module.exports.getCollege = getCollege
---
+++ collegeIntership_project_Nodejs/src/Controller/internController.js
@@ -0,0 +1,50 @@
+const internModel = require("../Model/internModel")
+const CollegeModel = require("../Model/CollegeModel")
+const validator = require("../validator/validate")
+
+
+
+//---------------------------------------------[ create intern]---------------------------------------------------------
+
+const CreateIntern = async function (req, res) {
+    try {
+        let data = req.body
+        // name
+        if (!data.name) return res.status(400).send({ status: false, message: "Name is required" });
+        if (!data.name.match(/^[a-zA-Z. ]{2,30}$/)) return res.status(400).send({ status: false, msg: "please enter valid intern name" })
+
+        // email
+        if (!(data.email)) return res.status(400).send({ status: false, message: "email is required" });
+        let email = req.body.email
+        if (!validator.isValidEmail(email)) return res.status(400).send({status:false, msg: `this Email is not valid ::${email}` })
+        let checkemail = await internModel.findOne({ email: data.email })
+        if (checkemail) return res.status(400).send({ status: false, msg: "email-ID Already Used" })
+
+        // mobile
+        if (!data.mobile) return res.status(400).send({ status: false, message: "mobile is required" });
+        let mobile = req.body.mobile
+        if (!validator.isValidNumber(mobile)) return res.status(400).send({ status:false,msg: `this mobile is not valid ::${mobile}` })
+        let checknumber = await internModel.findOne({ mobile: data.mobile })    /*Check Mobile From DB*/
+        if (checknumber) return res.status(400).send({ status: false, msg: "Mobile Number Already Used" })
+
+        // collegeID
+        if (!data.collegeName) return res.status(400).send({ status: false, message: "collegeName is required" });
+        const validateId = await CollegeModel.findOne({ name: data.collegeName })
+        if (!validateId) return res.status(400).send({ status: false, msg: "college is not valid or not exist" })
+        //----------------------------------
+        req.body.collegeId = validateId._id
+        await internModel.create(data)
+        let obj = {
+            isDeleted: false,
+            name: data.name,
+            email: data.email,
+            mobile: data.mobile,
+            collegeId: validateId._id
+        }
+        res.status(201).send({ status: true, msg: obj })
+    } catch (err) {
+        res.status(500).send({ msg: err.message })
+    }
+};
+
+module.exports.CreateIntern = CreateIntern
